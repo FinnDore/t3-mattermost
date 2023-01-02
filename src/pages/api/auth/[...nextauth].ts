7a00efc3e6b21803efbd88f99179f115b08d867d
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-02 19:34:27 +0000

    added oauth provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,66 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
+import type { OAuthConfig } from "next-auth/providers/oauth.js";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+
+const mattermostProvider = ({
+    mmUrl,
+    client_id,
+    client_secret,
+    callbackUri,
+}: {
+    mmUrl: string;
+    client_id: string;
+    client_secret: string;
+    callbackUri: string;
+}) =>
+    ({
+        id: "mattermost",
+        name: "Mattermost",
+        type: "oauth",
+        version: "2.0",
+        token: {
+            // For some reason its necessary to inline these values
+            url: `${mmUrl}/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}`,
+        },
+        accessTokenUrl: `${mmUrl}/oauth/access_token`,
+        authorization: {
+            url: `${mmUrl}/oauth/authorize`,
+            params: {
+                redirect_uri: callbackUri,
+                client_id,
+                client_secret,
+            },
+        },
+        userinfo: {
+            url: `${mmUrl}/api/v4/users/me`,
+            async request({ tokens }) {
+                const profile = await fetch(
+                    new URL(`${mmUrl}/api/v4/users/me`),
+                    {
+                        headers: {
+                            Authorization: `Bearer ${tokens.access_token}`,
+                        },
+                    }
+                ).then(async (res) => await res.json());
+                return profile;
+            },
+        },
+        profile(profile) {
+            return {
+                id: profile.id,
+                name: profile.username,
+                email: profile.email,
+            };
+        },
+    } satisfies OAuthConfig<{
+        id: string;
+        name: string;
+        email: string;
+        username: string;
+    }>);
 
 export const authOptions: NextAuthOptions = {
     // Include user.id on session
@@ -19,9 +75,11 @@ export const authOptions: NextAuthOptions = {
     // Configure one or more authentication providers
     adapter: PrismaAdapter(prisma),
     providers: [
-        DiscordProvider({
-            clientId: env.DISCORD_CLIENT_ID,
-            clientSecret: env.DISCORD_CLIENT_SECRET,
+        mattermostProvider({
+            mmUrl: env.MM_URL,
+            client_id: env.MM_CLIENT_ID,
+            client_secret: env.MM_CLIENT_SECRET,
+            callbackUri: env.MM_CALLBACK_URI,
         }),
         // ...add more providers here
     ],
